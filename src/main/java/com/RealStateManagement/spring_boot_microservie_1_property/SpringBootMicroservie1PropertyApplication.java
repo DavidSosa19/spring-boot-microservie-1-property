dfd332d7242c22ae6e18eedfba30b0a3966e34f0
DavidSosa <davidsosa32.22@gmail.com>
2025-01-12 11:33:55 -0500

    first commit

---
+++ src/main/java/com/RealStateManagement/spring_boot_microservie_1_property/SpringBootMicroservie1PropertyApplication.java
@@ -0,0 +1,13 @@
+package com.RealStateManagement.spring_boot_microservie_1_property;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMicroservie1PropertyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootMicroservie1PropertyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/RealStateManagement/spring_boot_microservie_1_property/SpringBootMicroservie1PropertyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.RealStateManagement.spring_boot_microservie_1_property;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootMicroservie1PropertyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
